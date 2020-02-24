9851882c399a12f0ae3a2df53f32a9396d7da99c
ivan_khomichkov <zloivan@mail.ru>
2020-02-24 15:07:51 +0300

    Initial commit

---
+++ Assets/FaceCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.forward = transform.position - target.position;
+        Debug.DrawRay(transform.position,transform.forward * 10, Color.red);
+    }
+}
