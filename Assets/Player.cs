8ae190b233a4b3268d2a933d9f840b030f9b7510
ivan_khomichkov <zloivan@mail.ru>
2020-02-27 17:43:27 +0300

    fixing camera and adding initial force to dragon

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   [SerializeField] private Rigidbody rigidbody;
+
+   private void Update()
+   {
+      rigidbody.AddForce(Vector3.right * 10f,ForceMode.Force);
+
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         //mylogs Probably remove this later
+         if (Debug.isDebugBuild) Debug.Log($"<color=purple>Pressing</color>");
+
+         rigidbody.AddForce(Vector3.up * 100f,ForceMode.Impulse);
+      }
+   }
+
+   
+}
